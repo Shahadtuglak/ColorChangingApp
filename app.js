7e6d0059e5b1dabc43115a88120fc9903574344a
Shahad Tuglak <56908101+Shahadtuglak@users.noreply.github.com>
2021-03-09 11:28:59 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function btn1(){
+    document.body.style.backgroundColor = "red";
+}
+function btn2(){
+    document.body.style.backgroundColor = "blue";
+}
+function btn3(){
+    document.body.style.backgroundColor = "green";
+}
+function btn4(){
+    document.body.style.backgroundColor = "yellow";
+}
+function btn5(){
+    document.body.style.backgroundColor = "white";
+}
